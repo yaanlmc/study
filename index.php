55f4ab70946894ccb963adf4a0e10042127ab039
yaanlmc <191856738@qq.com>
2019-04-15 21:05:21 +0800

    建立mysql 连接

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
 <?php
+//echo "<hr/>";
+//echo "hello world my first project";
+//echo "<br/>";
+////phpinfo();
+
+echo "你好吗？";
+header("content-type:text/html;charset=utf-8")
+$link = mysqli_connect('127.0.0.1','root','root');
+var_dump($link);
+?>
